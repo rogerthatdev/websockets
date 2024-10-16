130fd6579768838ef61fda1e77bbc2540f165c55
Roger Martinez <roger.that.dev@gmail.com>
2024-10-15 22:02:13 -0700

    have client generate a random string as an id

--- client.ts
+++ client.ts
@@ -1,9 +1,17 @@
+const { randomBytes } = await import('node:crypto')
 const ws = new WebSocket("ws://localhost:8000");
-const data = {name: "user1"}
+
+const generateId = () => {
+  return randomBytes(5).toString('hex').slice(0, 5);
+}
+
 ws.onopen = () => {
+  const id = generateId()
+  const data = {id: id}
   console.log("Connected to WebSocket server");
   ws.send(JSON.stringify(data));  // Send a "ping" message to the server
 };
+// TODO: make this client interactive
 
 ws.onmessage = (event) => {
   console.log("Received from server:", event.data);  // Log the response
