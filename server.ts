cb442ed1db82eb30644ce457cb7707b88c8a41d7
Roger Martinez <roger.that.dev@gmail.com>
2024-10-19 17:40:09 -0700

    clean up to server.ts - types and error catch

--- server.ts
+++ server.ts
@@ -5,20 +5,28 @@ const handler: (req: Request) => Response | Promise<Response> = (req) => {
   }
 
   // Upgrade the HTTP request to a WebSocket connection
-  const { socket, response } = Deno.upgradeWebSocket(req);
+  const { socket, response }: {socket: WebSocket, response: Response} = Deno.upgradeWebSocket(req);
 
   // Listen for "message" events on the WebSocket connection
-  socket.addEventListener("message", (event) => {
+  socket.addEventListener("message", (event: MessageEvent) => {
     console.log("Client:", event.data);
-    if (JSON.parse(event.data).id != undefined) {
-      const clientId = JSON.parse(event.data).id;
-      const msg = `welcome ${clientId}`;
-      socket.send(msg);
-      console.log(`Server: ${msg}`);
-    } else {
-      socket.send(`no ID provided. Closing.`);
+    // A try block go catch malformed incoming messages.
+    try {
+      if (JSON.parse(event.data).id != undefined) {
+        const clientId: string = JSON.parse(event.data).id;
+        const msg: string = `welcome ${clientId}`;
+        socket.send(msg);
+        console.log(`Server: ${msg}`);
+      } else {
+        socket.send(`no ID provided. Closing.`);
+        socket.close();
+      }
+    } catch (error) {
+      console.error("Could not parse message data: ", error);
+      socket.send("Invalid data format. Closing...");
       socket.close();
     }
+     
   });
 
   // Return the WebSocket response, completing the upgrade
